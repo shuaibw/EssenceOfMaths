f4ebadea99042c33ee83e8e0eea3f9b4b35e6031
shuaibw <1805010@ugrad.cse.buet.ac.bd>
2021-10-07 21:11:49 +0600

    Add wave ripple, color, near final render

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ class Animate(Scene):
     def construct(self):
         axes = Axes((-3, 10), (-1, 8), axis_config={
             "include_tip": False,
+            "stroke_width":2,
+            "tick_size":0.04
         })
+        axes.set_stroke(opacity=10)
+        axes.save_state()
         axes.add_coordinate_labels()
-
         self.play(Write(axes, lag_ratio=0.01, run_time=1))
         sin_graph = axes.get_graph(
             lambda x: 2 * math.sin(x),
@@ -99,10 +102,10 @@ class Animate(Scene):
 
         # self.play(left_tracker.animate.set_value(-2), right_tracker.animate.set_value(0), run_time=3)
         self.wait()
-        dx_list = [1, 0.5, 0.25, 0.1, 0.05, 0.025]
+        dx_list = [1, 0.5, 0.25, 0.1, 0.05, 0.025, 0.01]
         rects_list = VGroup(
             *[
-                axes.get_riemann_rectangles(graph=poly_graph, x_range=[left_end, right_end + .01], stroke_width=0.02,
+                axes.get_riemann_rectangles(graph=poly_graph, x_range=[left_end, right_end + .01], stroke_width=0,
                                             stroke_color=GREEN,
                                             dx=dx, fill_opacity=0.6)
                 for dx in dx_list
@@ -165,11 +168,19 @@ class Animate(Scene):
             last_brace = f_next_brace
             last_area_in_text = new_area_in_text
         tex_rect = SurroundingRectangle(VGroup(*area_tex_list[1:]))
-        tex_rect.set_stroke(BLUE, 2)
+        tex_rect.set_stroke(BLUE, 2.5)
         self.play(ShowCreation(tex_rect))
-        show_area_sum = Tex(r'\text{Area} \approx\sum_{i=1}^{n}f(x_i)\Delta x', font_size=35)
+        show_area_sum = Tex(r'Area ', r'\approx', r'\sum\limits_', r'{i=1}^', r'{n}', r'f(x_i)',
+                            r'\Delta x',
+                            font_size=35)
+        show_area_sum[4].set_color(BLUE)
+        show_area_sum[2].set_color(GREEN)
+        # show_area_sum[5].set_color(YELLOW)
+        # show_area_sum[6].set_color(ORANGE)
+        # sas = TexText(r'Area ', r'$\approx$', r'$\sum_{i=1}^{n}$', r'$f(x_i)$', r'$\Delta x$', font_size=35)
         show_area_sum.next_to(tex_rect, DOWN)
         self.play(ReplacementTransform(tex_rect, show_area_sum), *list(map(FadeOut, area_tex_list)))
+        # self.play(Transform(show_area_sum[0], TexText(r'$\int_a^b$')))
         n_tex, n_value = n_label = VGroup(
             Tex(r'n='),
             DecimalNumber(
@@ -187,20 +198,67 @@ class Animate(Scene):
         dx_label.arrange(RIGHT)
         n_label.arrange(RIGHT)
         n_dx_grp = VGroup(n_label, dx_label).arrange(DOWN)
-        n_dx_grp.to_edge(RIGHT_SIDE).shift(RIGHT/1.8)
+        n_dx_grp.to_edge(RIGHT_SIDE).shift(RIGHT / 1.8)
         self.play(Write(n_dx_grp))
 
-        # self.play(show_area_sum.animate.shift(UP))
         to_remove += [last_area_in_text, last_brace, last_brace.label, dx_brace_group]
         self.play(*(list(map(FadeOut, to_remove))))
-        # self.embed()
+        int_start = TexText(r'$a$').set_color(BLUE)
+        int_end = TexText(r'$b$').set_color(GREEN)
+        int_start.move_to(axes.c2p(2, -.5))
+        int_end.move_to(axes.c2p(6, -.5))
+        self.play(axes.animate.restore(), Write(int_start), Write(int_end))
         for k in range(1, len(dx_list)):
             new_approx = rects_list[k]
-            self.play(Transform(first_approx, new_approx), FadeOut(pieces), run_time=1)
+            self.play(Transform(first_approx, new_approx), FadeOut(pieces),
+                      n_value.animate.set_value(len(new_approx)),
+                      dx_value.animate.set_value(dx_list[k]),
+                      run_time=1)
             pieces = self.create_const_lines(2, 6, dx_list[k], axes, poly_graph, color=YELLOW, opacity=1)
             self.play(FadeIn(pieces), lag_ratio=0.5, time=1)
             self.wait(0.5)
+        self.play(show_area_sum.animate.shift(UP))
+        sum_rect = SurroundingRectangle(show_area_sum[2:5])
+        sum_rect.set_stroke(BLUE, 2.5)
+        integral_sign = Tex(r'\int', r'_a', r'^b', r'f(x)', r'\,dx', font_size=35)
+        integral_sign[2].set_color(BLUE)
+        integral_sign[1].set_color(GREEN)
+        # integral_sign[3].set_color(YELLOW)
+        # integral_sign[4].set_color(ORANGE)
+        integral_sign.next_to(sum_rect, DOWN)
+        self.play(ShowCreation(sum_rect))
+        self.play(TransformFromCopy(show_area_sum[2:5], integral_sign[0:3]), Uncreate(sum_rect), run_time=1.5)
+        fx_rect = SurroundingRectangle(show_area_sum[5])
+        dx_rect = SurroundingRectangle(show_area_sum[6])
+        fx_rect.set_stroke(BLUE, 2.5)
+        dx_rect.set_stroke(BLUE, 2.5)
+        self.play(ShowCreation(fx_rect))
+        self.play(TransformFromCopy(show_area_sum[5], integral_sign[3]), Uncreate(fx_rect))
+        self.play(ShowCreation(dx_rect))
+        self.play(TransformFromCopy(show_area_sum[6], integral_sign[4]), Uncreate(dx_rect))
+        self.play(ApplyWave(rects_list[-1], rate_func=linear, amplitude=0.7, time_width=0.1, ripples=4), run_time=3)
+        int_rect = SurroundingRectangle(integral_sign)
+        int_rect.set_stroke(GREEN, 2.5)
+        summary = TexText(r'Area under the curve ', r'$f(x)$\\', r' from ', r'$x=$', r'$a$', ' to ', r'$x=$', r'$b$',
+                          font_size=32)
+        summary[4].set_color(BLUE)
+        summary[7].set_color(GREEN)
+        summary.next_to(int_rect, DOWN)
+        self.play(ShowCreation(int_rect), run_time=2)
+        self.play(Write(summary[0:2]))
+        self.play(CircleIndicate(summary[1], color=YELLOW, run_time=1))
+        self.play(Write(summary[2:5]))
+        self.play(CircleIndicate(summary[4], color=BLUE, run_time=1))
+        self.play(Write(summary[5:]))
+        self.play(CircleIndicate(summary[7], color=GREEN, run_time=1))
+        self.play(Uncreate(int_rect))
         self.wait()
+        self.play(
+            *[FadeOut(mob) for mob in self.mobjects]
+        )
+        thanks = Text('Thank you for watching!', font_size=35)
+        thanks.set_color_by_gradient(BLUE, GREEN)
+        self.play(Write(thanks))
 
     def create_const_lines(self, start, end, step, axes, graph, **kwargs):
         pieces = VGroup()
